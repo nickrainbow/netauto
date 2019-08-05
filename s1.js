b84610813e2dd4125fafae754044d438680062da
Nick <nick@email.com>
2019-08-05 11:33:57 -0400

    updated func

--- s1.js
+++ s1.js
@@ -1,15 +1,31 @@
 function add(a, b) {
-  const { a, b } = checkType( [a, b] );
+  [ a, b ] = checkType( [a, b] );
   return a + b;
 }
 
 
-function checkType(args) {
-  if ( args.map ) {
-    args.map( el => {
-      checkType( el );
-    } );
-  }
+function checkType(arg) {
+  let output;
   
-  //  todo
+  if ( arg.map ) {
+    output = arg.map( el => {
+      return checkType( el );
+    } );
+  } else {
+  
+    arg = Number( arg );
+    if ( !Number.isNaN(arg) ) {
+      throw new TypeError( 'The argument is not convertable to number' );
+    }
+
+  }
+
+  if ( output ) {
+    return output;
+  }
+
+  return arg;
 }
+
+
+console.log( add( 1, 3) );
